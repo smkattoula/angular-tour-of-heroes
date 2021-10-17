2a19131199a499ba363152b3144ec0d3dbbe25f1
Shaker Kattoula <smkattoula@gmail.com>
2021-10-17 16:52:09 -0700

    create get requests via HttpClient in HeroService for getHeroes and getHero methods, add piping tto error handlers in the service methods, use the tap method to display a message if the request is successful, otherwise pipe to the catchError method and pass in the error handler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,8 +13,22 @@ import { MessagesComponent } from './components/messages/messages.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service';
 import { Observable, of } from 'rxjs';
 import { HEROES } from '../mock-heroes';
@@ -8,17 +10,42 @@ import { Hero } from '../Hero';
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = 'api/heroes';
 
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
+
+  // log a HeroService message with the MessageService
+  private log(message: string) {
+    this.messageService.add(`Hero Service: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
+  // get all heroes
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('Hero Service: fetched heroes');
+    const heroes = this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
     return heroes;
   }
 
+  // get a single hero
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find((hero) => hero.id === id);
-    this.messageService.add(`Hero Service: fetched hero id = ${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    const hero = this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`fetched hero id = ${id}`)),
+      catchError(this.handleError<Hero>('getHero id = ${id}'))
+    );
+    return hero;
   }
 }
